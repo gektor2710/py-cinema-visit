41a67ba9215311ac7b182e1b61ba2372586e57dd
Mykola Kobzarov <gektor2710@gmail.com>
2026-09-12 21:24:57 +0200

    Implement cinema visit feature

--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
+    customers_list = []
+
+    for customer_data in customers:
+        customer = Customer(
+            name=customer_data["name"],
+            food=customer_data["food"]
+        )
+
+        CinemaBar.sell_product(
+            product=customer.food,
+            customer=customer,
+        )
+
+        customers_list.append(customer)
+
+    hall = CinemaHall(number=hall_number)
+    cleaner = Cleaner(name=cleaner)
+
+    hall.movie_session(
+        movie_name=movie,
+        customers=customers_list,
+        cleaning_staff=cleaner,
+    )
---
+++ app/people/customer.py
@@ -0,0 +1,10 @@
+from builtins import print
+
+
+class Customer:
+    def __init__(self, name: str, food: str) -> None:
+        self.name = name
+        self.food = food
+
+    def watch_movie(self, movie: str) -> None:
+        print(f'{self.name} is watching "{movie}".')
